ef889b30f694a572bb81467dd94feab0f7afbf2e
devdewa <uchiasasuke123.sp@gmail.com>
2024-04-05 21:27:45 +0700

    chore: new index pages

---
+++ src/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="flex justify-center items-center h-dvh gap-12">
+    <NuxtImg src="/logo/laravel.svg" alt="" aria-label="" class="size-20" />
+    <UButton :padded="false" color="gray" variant="link" icon="i-heroicons-x-mark-20-solid" to="/posts" />
+    <NuxtImg src="/logo/nuxt.svg" alt="" aria-label="" class="size-20" />
+  </section>
+</template>
