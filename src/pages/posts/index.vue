95467366383de1b4578b623645fb849e28ca3da6
devdewa <uchiasasuke123.sp@gmail.com>
2024-04-07 19:54:44 +0700

    chore: update table styling

--- src/pages/posts/index.vue
+++ src/pages/posts/index.vue
@@ -24,44 +24,38 @@ async function deletePost(id: number) {
     <UButton to="/posts/create" color="sky" variant="soft" class="btn btn-md btn-success rounded shadow border-0 mb-3">
       Add Post
     </UButton>
-    <div class="card border-0 rounded shadow">
-      <div class="card-body">
-        <table class="table table-bordered w-full">
-          <thead class="text-white">
-            <tr>
-              <th scope="col">
-                Image
-              </th>
-              <th scope="col">
-                Title
-              </th>
-              <th scope="col">
-                Content
-              </th>
-              <th scope="col" style="width:15%">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(post, index) in posts.data.data" :key="index">
-              <td>
-                <NuxtImg :src="post.image" class="size-80 rounded-xl mx-auto" />
-              </td>
-              <td class="text-center">
-                {{ post.title }}
-              </td>
-              <td class="text-center">
-                {{ post.content }}
-              </td>
-              <td class="space-x-4 flex">
-                <UButton icon="i-heroicons-pencil-square-16-solid" size="sm" color="yellow" variant="soft" :to="`/posts/edit/${post.id}`" />
-                <UButton icon="i-heroicons-trash" size="sm" color="red" variant="soft" @click="deletePost(post.id)" />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <table class="border-collapse table-fixed w-full">
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(post, index) in posts.data.data" :key="index">
+          <td>
+            <NuxtImg :src="post.image" class="size-20 rounded-xl mx-auto" />
+          </td>
+          <td class="text-center">
+            {{ post.title }}
+          </td>
+          <td class="text-center">
+            {{ post.content }}
+          </td>
+          <td class="space-x-4 text-center">
+            <UButton icon="i-heroicons-pencil-square-16-solid" size="sm" color="yellow" variant="soft" :to="`/posts/edit/${post.id}`" />
+            <UButton icon="i-heroicons-trash" size="sm" color="red" variant="soft" @click="deletePost(post.id)" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<style scoped>
+td {
+  padding: 1.5rem 0;
+}
+</style>
